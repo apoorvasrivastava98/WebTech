731038fa735131cf048e77ccc80b763d00d0d3d5
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-10-08 12:29:16 +0530

    Add files via upload

---
+++ 8-10-20(OperationOnTwoNumbers).php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+	<h1>Sum of two numbers</h1>
+<?php
+	$a=26;
+    echo "<br>First Number=",$a;
+    $b=14;
+    echo "<br>Second Number=",$b;
+    $s=$a+$b;
+    echo "<br>Sum of two numbers=",$s;
+?>
+<h1>Difference of two numbers</h1>
+<?php
+	$a=26;
+    echo "<br>First Number=",$a;
+    $b=14;
+    echo "<br>Second Number=",$b;
+    $s=$a-$b;
+    echo "<br>Difference of two numbers=",$s;
+?>
+<h1>Multiplication of two numbers</h1>
+<?php
+	$a=26;
+    echo "<br>First Number=",$a;
+    $b=14;
+    echo "<br>Second Number=",$b;
+    $s=$a * $b;
+    echo "<br>Multiplication of two numbers=",$s;
+?>
+<h1>Division of two numbers</h1>
+<?php
+	$a=100;
+    echo "<br>First Number=",$a;
+    $b=10;
+    echo "<br>Second Number=",$b;
+    $s=$a/$b;
+    echo "<br>Division of two numbers=",$s;
+?>
+</body>
+</html>
