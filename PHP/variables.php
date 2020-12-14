4c15df40cc7c5007fab1c923ed76140716c7d7dd
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-12-14 23:08:05 +0530

    Add files via upload

---
+++ PHP/variables.php
@@ -0,0 +1,28 @@
+<! -- 6/10/2020 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Variables in php</title>
+</head>
+<body>
+<?php
+$name="PHP";
+echo "<br>";
+var_dump($name);
+echo "<br>";
+$number=1000;
+var_dump($number);
+$number=12.334;
+echo "<br>";
+var_dump($number);
+$number=true;
+echo "<br>";
+var_dump($number);
+$number="KIET";
+echo "<br>";
+var_dump($number);
+echo "<br>This is PHP","This is another line","This is 3rd line";
+print "<br>This is PHP"."This is another line";
+?>
+</body>
+</html>
