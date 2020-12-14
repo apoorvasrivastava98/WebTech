09e36455ec2f2685a73ead1436e3f84d7861a062
Apoorva Srivastava <55443541+apoorvasrivastava98@users.noreply.github.com>
2020-12-14 23:13:57 +0530

    Add files via upload

---
+++ PHP/sum.php
@@ -0,0 +1,15 @@
+<! -- 6/10/2020 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Sum in php</title>
+</head>
+<body>
+<?php
+$num1=100;
+$num2=100;
+echo "Sum of two numbers=",$num1+$num2;
+print "<br>Sum of two numbers=".($num1+$num2);
+?>
+</body>
+</html>
